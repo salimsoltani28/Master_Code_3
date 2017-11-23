53894bdec522359d0a3e79880b239c50d1392f7d
salimsoltani28 <32277505+salimsoltani28@users.noreply.github.com>
2017-11-23 17:20:58 +0430

    Update my today's code excersize

--- Remote_Sensing_4Ecologist.R
+++ Remote_Sensing_4Ecologist.R
@@ -60,6 +60,48 @@ eeResults<- readEE(eeResults)
 print(readEE)
 install.packages("ggplot")
 library(ggplot2)
+
 # Bring up your climate raster data to work in in R ( I am excited doing that for the first time)
-ggplot2(subset(eeResults, year< 2015 & cloud.Cover<20))+ geom_tile(aes(x= Doy, Y= Year, alpha= cloud.Cover, fill= Satellite), width= 2 ,size= 2)+ 
+
+ggplot(subset(eeResults, year< 2015 & cloud.Cover<20))+ geom_tile(aes(x= Doy, Y= Year, alpha= cloud.Cover, fill= Satellite), width= 2 ,size= 2)+ 
   scale_y_continuous(breaks = c(1984:2014))+ scale_alpha_continuous(name= "cloud cover(%)",range = c(1,0.5))
+
+
+#Access data from open street map
+library(osmar)
+install.packages("osmar")
+study_area_bb<- center_bbox(-49.75, -4.72, 30000, 30000)
+OSMdata<- get_osm(study_area_bb, source = osmsource_api())
+plot(OSMdata)
+
+#import data raster with one band
+library("raster")
+#insert the single band using (raster) function 
+p224r63_2011_B1<- raster("F:/03Projects/Remote Sensing for Eco-Logist/data_book/raster_data/LT52240632011210/LT52240632011210CUB01_B1.tif")
+p224r63_2011_B1
+Band2<- raster("F:/03Projects/Remote Sensing for Eco-Logist/data_book/raster_data/LT52240632011210/LT52240632011210CUB01_B2.tif")
+Band3<- raster("F:/03Projects/Remote Sensing for Eco-Logist/data_book/raster_data/LT52240632011210/LT52240632011210CUB01_B3.tif")
+Band4<- raster("F:/03Projects/Remote Sensing for Eco-Logist/data_book/raster_data/LT52240632011210/LT52240632011210CUB01_B4.tif")
+Band5<- raster("F:/03Projects/Remote Sensing for Eco-Logist/data_book/raster_data/LT52240632011210/LT52240632011210CUB01_B5.tif")
+Band6<- raster("F:/03Projects/Remote Sensing for Eco-Logist/data_book/raster_data/LT52240632011210/LT52240632011210CUB01_B6.tif")
+Band7<- raster("F:/03Projects/Remote Sensing for Eco-Logist/data_book/raster_data/LT52240632011210/LT52240632011210CUB01_B7.tif")
+plot(Band3)
+#stack all Band together 
+p224r63_2011<- stack(p224r63_2011_B1,Band2,Band3,Band4,Band5,Band6,Band7)
+plot(p224r63_2011)
+#easily import all bands with one function 
+allband<- list.files("F:/03Projects/Remote Sensing for Eco-Logist/data_book/raster_data/All_BandPractice",all.files = FALSE, full.names = TRUE)
+# then stack together 
+allband
+p224r63_2011<- stack(allband)
+#Rename or possibly remove some part of names 
+names(p224r63_2011)<- gsub(pattern = "LT52240632011210CUB01_","_",x=names)
+p224r63_2011
+
+names(p224r63_2011)<- gsub(pattern = "LT52240632011210CUB01_","", x=names(p224r63_2011))
+p224r63_2011
+#Save the raster in one single Band through use of the symbol
+writeRaster(p224r63_2011, filename = "F:/03Projects/Remote Sensing for Eco-Logist/data_book/raster_data/All_BandPractice/LT52240632011210.tif")
+# import all bands from a multi band file use the belwo function
+p224r63<- brick("F:/03Projects/Remote Sensing for Eco-Logist/data_book/raster_data/final/p224r63_2011.grd")
+inMemory(p224r63)
